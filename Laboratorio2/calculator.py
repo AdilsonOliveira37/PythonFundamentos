fea2b69d31679e645f4280f320af105808c0ffd5
Adilson <adilsonreisoliveira.98@gmail.com>
2020-07-22 18:33:58 -0300

    Laboratório-02

--- Laboratorio2/calculator.py
+++ Laboratorio2/calculator.py
@@ -16,17 +16,17 @@ num1 = float(input("Insira o primeiro número:"))
 num2 = float(input("Insira o segundo número:"))
 
 if (operacao == 1): 
-    print(soma(num1,num2))
+    print(num1, "+", num2, "= ", soma(num1,num2))
     
 elif(operacao == 2):
-    print(subtracao(num1,num2))
+    print(num1, "-", num2, "= ", subtracao(num1,num2))
 
 elif(operacao == 3 ):
-    print(multiplicacao(num1,num2))
+    print(num1, "*", num2, "= ", multiplicacao(num1,num2))
 
 elif(operacao == 4):
     if(num2 != 0):
-        print(divisao(num1,num2))
+        print(num1, "/", num2, "= ", divisao(num1,num2))
     print("Divisão por 0")
 
 else: 
